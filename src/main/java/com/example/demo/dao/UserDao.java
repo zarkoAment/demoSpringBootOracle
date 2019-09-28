d340907bf84b9c8bda9608599aa5cf4367c43d1d
zarko777 <zarko.ament@gmail.com>
2019-09-28 20:12:38 +0200

    Springboot demo

---
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.UserDao;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+
+    public List<User> getAllUsers() {
+        return this.userDao.findAll();
+    }
+
+    public User addUser(User user) {
+        return this.userDao.save(user);
+    }
+
+    public Optional<User> getUserById(int id) {
+        return this.userDao.findById(id);
+    }
+
+    public User updateUser(User user) {
+        return this.userDao.save(user);
+    }
+
+    public void deleteUserById(int id) {
+        this.userDao.deleteById(id);
+    }
+
+    public void deleteAllUsers() {
+        this.userDao.deleteAll();
+    }
+}
